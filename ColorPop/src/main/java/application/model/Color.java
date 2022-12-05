785fc90f60158fbbc0236178bffa2701cc9862e7
Francesco Copelli <75535464+francescocopelli@users.noreply.github.com>
2022-12-06 00:29:44 +0100

    Update Color.java
    Added final to internal variable
    Create facade for getRGB method

--- ColorPop/src/main/java/application/model/Color.java
+++ ColorPop/src/main/java/application/model/Color.java
@@ -1,28 +1,38 @@
 package application.model;
 
 public enum Color {
-   RED(1, "#FF0000"), GREEN(2, "#00FF00"), BLUE(3, "#0000FF"), YELLOW(4, "#FFFF00"), EMPTY(0, "#FFFFFF");
+	RED(1, "#FF0000"), GREEN(2, "#00FF00"), BLUE(3, "#0000FF"), YELLOW(4, "#FFFF00"), EMPTY(0, "#FFFFFF");
 
-    public int number;
-    public String color;
+	private final int number;
+	private final String color;
 
-    Color(int number, String color) {
-        this.number = number;
-        this.color = color;
-    }
+	Color(int number, String color) {
+		this.number = number;
+		this.color = color;
+	}
 
-    public int getNumber() {
-        return number;
-    }
+	public int getNumber() {
+		return number;
+	}
 
-    public static Color getColor(int number) {
-        for (Color color : Color.values()) {
-            if (color.number == number) {
-                return color;
-            }
-        }
-        return null;
-    }
+	public static String getRGB(int number) {
+		for (Color color : Color.values()) {
+			if (color.number == number) {
+				return color.color;
+			}
+		}
+		return Color.EMPTY.color;
+	}
+
+	public static String getRGB(Color color) {
+		return getRGB(color.number);
+	}
+
+	public static Color getColor(int number) {
+		for (Color color : Color.values())
+			if (color.number == number) return color;
+		return Color.EMPTY;
+	}
 
 
 }
