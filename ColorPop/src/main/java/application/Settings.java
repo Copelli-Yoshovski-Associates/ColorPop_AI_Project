a1fb5a6d0daa9e9a019408c43c09b901aaf1827c
Francesco Copelli <75535464+francescocopelli@users.noreply.github.com>
2022-12-05 22:20:13 +0100

    Update Settings.java
    - Autodetermine colors based on enum class
    - Fixed TOTAL_COLORS type

--- ColorPop/src/main/java/application/Settings.java
+++ ColorPop/src/main/java/application/Settings.java
@@ -1,13 +1,13 @@
 package application;
 
+import application.model.Color;
+
 public class Settings {
 
-    public static final int DEFAULT_HEIGHT = 600;
-    public static final int DEFAULT_WIDTH = 600;
-
-    public static final int ROWS = 10;
-    public static final int COLUMNS = 8;
-	public static final double TOTAL_COLORS = 4;
-    public static final String MAX_TIME = "10";
-    
+	public static final int DEFAULT_HEIGHT = 600;
+	public static final int DEFAULT_WIDTH = 600;
+	public static final int ROWS = 10;
+	public static final int COLUMNS = 8;
+	public static final int TOTAL_COLORS = Color.values().length-1;
+	public static final String MAX_TIME = "10";
 }
