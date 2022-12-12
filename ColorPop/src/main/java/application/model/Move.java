63d822b8bfe801016eae0c960f8a39099b58dc56
yoshovski <stefan.yoshovski@gmail.com>
2022-12-12 10:48:27 +0100

    move class

---
+++ ColorPop/src/main/java/application/model/Move.java
@@ -0,0 +1,49 @@
+package application.model;
+
+
+import it.unical.mat.embasp.languages.Id;
+import it.unical.mat.embasp.languages.Param;
+
+@Id("inMossa")
+public class Move implements java.io.Serializable {
+
+    @Param(0)
+    private int x;
+    @Param(1)
+    private int y;
+    @Param(2)
+    private int c;
+
+    public Move() {
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public Move(int x, int y, int c) {
+        this.x = x;
+        this.y = y;
+        this.c = c;
+    }
+}
